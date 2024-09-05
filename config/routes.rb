924cb235f2fcff1730fff8babcf902cca215b930
mauricio <mchirino@aniracloud.com>
2024-09-05 18:35:14 -0400

    con el project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "pages/home"
-  get "pages/projects"
-  get "pages/contact"
+  get "pages/projects", to: 'pages#projects'
+  get "pages/contact", to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
